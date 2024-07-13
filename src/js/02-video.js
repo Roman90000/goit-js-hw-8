b543e3c82501d2c89ef442f5d72c49fe04625c26
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-07-13 23:15:44 +0300

    task 2 ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,15 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+
+const KEY_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  localStorage.setItem(KEY_TIME, JSON.stringify(evt.seconds));
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_TIME)));
