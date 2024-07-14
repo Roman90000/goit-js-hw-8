4cdb5c09a46db04a75e1052a079d0bb3e80a2699
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-07-14 12:19:01 +0300

    task 3 ready

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+loadingPage();
+
+let dataForm = {};
+
+function onInput(e) {
+  dataForm[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataForm));
+}
+
+function loadingPage() {
+  const localData = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseData = JSON.parse(localData);
+
+  if (parseData) {
+    formEl[0].value = parseData.email || '';
+    formEl[1].value = parseData.message || '';
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  if (!formEl[0].value || !formEl[1].value) {
+    return alert(`Будь ласка, заповніть всі обов'язкові поля.`);
+  }
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  document.querySelector('.feedback-form').reset();
+}
